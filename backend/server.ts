405400e9e4bdb2e45927c8d9128305bbc73d0095
Ishir Garg <gargishir@gmail.com>
2024-10-19 19:50:55 -0700

    twilio to proxy

--- backend/server.ts
+++ backend/server.ts
@@ -6,24 +6,12 @@ const app = express();
 app.use(express.json());
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
-app.post("/twiml", (req, res) => {
-  res.set("Content-Type", "text/xml");
-  res.send(`
-    <Response>
-      <Start>
-        <Stream url="wss://${req.headers.host}/"/>
-      </Start>
-      <Say>Hi this is AI customer care services speaking. How can I help you?</Say>
-      <Pause length="120" />
-    </Response>
-  `);
-});
+
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log("LISTENING")
-  //console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 });
-//set 5mins
+
 server.setTimeout(300000);
 
 //handling websocket
